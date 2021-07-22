9bf9a780c5a169e23d23598cb89ba570c18b2fa5
Nissa2424 <Nissa2424@gmail.com>
2021-07-21 20:42:52 -0500

    arrays-loops11

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("Nissa");
+
+let group6 = [
+    {
+        id: 1,
+        name: "Nissa",
+        email: "nissa@nissian.org",
+        favoriteColor: "pink",
+        uniqueDesc: "I like stilletos",
+        classPhoto: https://avatars.githubusercontent.com/u/86807857?v=4,
+    },
+    {
+        id: 2,
+        name: "Daniel",
+        email: "daniel#email.com",
+        favoriteColor: "Teal",
+        uniqueDesc: "I build PCs, 
+        classPhoto: https://avatars.githubusercontent.com/u/82732748?v=4,
+    },
+    {
+        id: 3,
+        name: "Halie",
+        email: "halie@email.com",
+        favoriteColor: "green",
+        uniqueDesc: "allergic to advil",
+        classPhoto: https://avatars.githubusercontent.com/u/86806913?v=4.
+    },
+    {
+        id: 4,
+        name: "Klay",
+        email: "klaythacker@email.com",
+        favoriteColor: "purple",
+        uniqueDesc: "has hearing loss",
+        classPhoto: https://avatars.githubusercontent.com/u/86626953?v=4,
+    },
+]
